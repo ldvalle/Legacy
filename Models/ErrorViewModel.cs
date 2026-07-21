2d95532674a6ddc5229355e4f7005036ff98b4bb
ldvalle <34348282+ldvalle@users.noreply.github.com>
2026-07-21 09:53:12 -0300

    actualizacion

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Legacy.Models;
+//para ver si toma el cambio
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
